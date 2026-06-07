85ebf5d1c8becf6209ee6190ea4ce35e75bd2c45
Lord_Zane021 <140982529+Lord-Zane1225@users.noreply.github.com>
2026-06-08 09:44:05 +1200

    incomplete csv reader

---
+++ C_04_Get_Question_Answer_Data_v1.py
@@ -0,0 +1,52 @@
+import csv
+import random
+
+def round_ans(val):
+    """
+    Rounds temperatures to nearest degree
+    :param val: number to be rounded
+    :return: number rounded to nearest degree
+    """
+    var_rounded = (val * 2 + 1) // 2
+    return "{:.0f}".format(var_rounded)
+
+
+# retrieve colours from csv file and put them in a list
+file = open("quiz/Aikido Quiz Database.csv", "r")
+all_questions = list(csv.reader(file, delimiter=","))
+file.close()
+
+# remove the first row
+all_questions.pop(0)
+
+answer_options = []
+colour_scores = []
+
+# pick a question
+quiz_question_chosen = random.choice(all_questions)
+
+# loop until we have four colours with different scores
+while len(answer_options) < 4:
+    potential_colour = random.choice(all_questions)
+
+    # get the score and check it's not a duplicate
+    if potential_colour[1] not in colour_scores:
+        answer_options.append(potential_colour)
+        colour_scores.append(potential_colour[1])
+
+print(answer_options)
+print(colour_scores)
+
+# find target score (median)
+
+# change scores to integers
+int_scores = [int(x) for x in colour_scores]
+print("scores unsorted", int_scores)
+int_scores.sort()
+print("scores sorted", int_scores)
+
+median = (int_scores[1] + int_scores[2]) / 2
+median = round_ans(median)
+print("median", median)
+
+
