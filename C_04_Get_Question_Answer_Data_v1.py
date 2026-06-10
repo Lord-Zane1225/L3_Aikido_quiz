0f6147e6109c7b7cd63a30ab6e663c862a017953
Lord_Zane021 <140982529+Lord-Zane1225@users.noreply.github.com>
2026-06-10 12:35:53 +1200

    gets 4 random questions

--- C_04_Get_Question_Answer_Data_v1.py
+++ C_04_Get_Question_Answer_Data_v1.py
@@ -2,7 +2,7 @@ import csv
 import random
 
 # retrieve colours from csv file and put them in a list
-file = open("quiz/Aikido Quiz Database.csv", "r")
+file = open("Aikido Quiz Database.csv", "r")
 all_questions = list(csv.reader(file, delimiter=","))
 file.close()
 
@@ -10,33 +10,11 @@ file.close()
 all_questions.pop(0)
 
 answer_options = []
-colour_scores = []
 
-# pick a question
-quiz_question_chosen = random.choice(all_questions)
-
-# loop until we have four colours with different scores
+# loop until we have four options
 while len(answer_options) < 4:
-    potential_colour = random.choice(all_questions)
-
-    # get the score and check it's not a duplicate
-    if potential_colour[1] not in colour_scores:
-        answer_options.append(potential_colour)
-        colour_scores.append(potential_colour[1])
+    potential_option = random.choice(all_questions)
+    answer_options.append(potential_option)
 
 print(answer_options)
-print(colour_scores)
-
-# find target score (median)
-
-# change scores to integers
-int_scores = [int(x) for x in colour_scores]
-print("scores unsorted", int_scores)
-int_scores.sort()
-print("scores sorted", int_scores)
-
-median = (int_scores[1] + int_scores[2]) / 2
-median = round_ans(median)
-print("median", median)
-
 
